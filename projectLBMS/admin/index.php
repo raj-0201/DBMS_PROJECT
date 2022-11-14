514879d6fa915e3a7e64fa5d61511f88fb9be3b9
Raj Telgote <rsdt210@gmail.com>
2022-11-14 11:02:30 +0530

    Initial commit

---
+++ projectLBMS/admin/index.php
@@ -0,0 +1,108 @@
+ <html>
+<head>
+<title>login</title>
+<style>
+div
+{
+background-image:url(images/1.jpg);
+height:220px;
+width:250px;
+border:4px thin red;
+border-radius:0px;
+margin-top:300px;
+margin-left:300px;
+float:left;
+ 
+
+
+
+
+}
+
+#a
+{
+
+height:665px;
+width:1340px;
+background-image:url(images/1.jpg);
+float:left;
+margin-top:0px;
+margin-left:0px;
+
+}
+#c
+{
+background-image:url(1.jpg);
+height:100px;
+width:100px;
+border:4px thin red;
+border-radius:0px;
+margin-top:355px;
+margin-right:380px;
+float:right;
+-webkit-transform:skew(15deg,0deg);
+
+
+}
+</style>
+
+</head>
+<body>
+
+<?php
+if(isset($_POST['s']))
+{
+	$a=$_POST['uname'];
+	$b=$_POST['pwd'];
+	
+	if($a=='admin' && $b=='admin')
+	{
+		header('location:main.php');
+		}
+	else
+	{
+		echo "wrong username & password";
+		}
+
+}
+
+?>
+<form action="index.php" method="post">
+
+<div id="a">
+
+<table align="left" >
+
+<tr>
+
+<td colspan="2"  >
+<div align="center">
+<br><br>
+
+<center>
+<input type="text" placeholder="username" name="uname" size="40" style="height:30px; width:200px; background-color:#FCBC66;"/><br><br>
+<input type="password" placeholder="password" name="pwd"size="40" style="height:30px; width:200px;background-color:#FCBC66;"/><br><br>
+<input type="checkbox"/>Remember password&nbsp;&nbsp;
+<input type="reset" value="RESET" style=" background-color:#FCBC66;"/><br><br>
+<a href=""><font color="#000000">Forgot Password??</font></a>
+</td>
+</center>
+</div>
+</tr>
+
+<div id="c">
+<br><br><center>
+<input type="submit" value="LOGIN" name="s"  style="height:50px; width:100px;background-color:#FCBC66; margin-top:-12px;"/>
+</center>
+</div>
+
+
+
+
+</table>
+</div>
+</form>
+<?php
+?>
+</body>
+</html>
---
+++ projectLBMS/user/login.php
@@ -0,0 +1,136 @@
+<?php
+
+session_start();
+
+
+?>
+<html>
+<head>
+<title>WELCOME</title>
+<style>
+div
+{
+background-image:url(images/white-damask-background_00435854.jpg);
+height:653.2px;
+width:1345px;
+margin-top:0px;
+
+}
+#a
+{
+background:#761816;
+height:40px;
+width:1345px;
+list-style:none;
+
+
+
+}
+
+ 
+#b
+{
+background-image:url(images/logo.png);
+height:100px;
+width:760px;
+float:left;
+margin-left:300px;
+border:2px dashed #761816;
+
+}
+#c
+{
+background:#761816;
+height:100px;
+width:400px;
+float:left;
+margin-left:500px;
+margin-top:130px;
+font:cursive;
+font-style:oblique;
+font-size:24px;
+color:#00FF00;
+}
+#d
+{
+background:#000000;
+height:200px;
+width:400px;
+}
+ 
+
+
+
+
+
+
+</style>
+</head>
+<body>
+
+
+<div>
+ 
+<div id="a">
+  
+</div><br />
+	<div id="b">
+	<br><br><br><br><br><center><p><font size="6" color="#0000CC">WELCOME TO COEP CENTRAL LIBRARY</font></p></center>
+	</div>
+	
+		<div id="c">
+		<center>
+		please login to coninue
+		</center>
+			<div id="d">
+				<table align="left" >
+				
+					<tr>
+					
+						<td colspan="2"   >
+						
+							<form action="" method="post" style="margin-top:30px;">
+								<input type="text" placeholder="username" name="uname" size="40" style="height:30px; width:220px; background-color:; margin-left:100px;"/><br><br>
+								<input type="password" placeholder="password" name="pwd"size="40" style="height:30px; width:220px;background-color:; margin-left:100px;"/><br><br>
+								<input type="submit" value="login" name="q" style=" background-color:; margin-left:95px; border-radius:20px; height:30px; width:80px;"/>
+								<input type="reset" value="RESET" style=" background-color:; margin-left:60px; border-radius:20px; height:30px; width:80px;"/>
+							</form> 
+						</td>
+					
+					</tr>
+				</table>
+			
+			
+			
+			</div>
+		</div>
+	</div>
+<?php
+if(isset($_POST['q']))
+{
+	$a=$_POST['uname'];
+	$b=$_POST['pwd'];
+	
+	include('connection.php');
+	$q=mysql_query("select * from `adduser` where `Name`='$a' and `Rollno`='$b'");
+	echo "select * from `adduser` where `Name`='$a' and `MIS`='$b'";
+	$y=mysql_num_rows($q);
+	if($y>0)
+	{
+		$_SESSION['uname']=$a;
+		$_SESSION['roll']=$b;
+		header('location:userdetails.php');
+	}	
+	else
+	{
+		echo "<script>
+			alert('username or password mismatch');
+			</script>";
+		}
+
+}
+
+?>
+
+</body>
+</html>
